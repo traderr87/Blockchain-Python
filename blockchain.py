0374cb9d0a5ab9ef0c3fc4c05ed88e3f67ed3e08
trader87 <ironbone98@gmail.com>
2025-08-20 19:13:40 +0700

    update_1

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,7 @@
 import datetime
 import json
 import hashlib
+from flask import Flask, jsonify
 
 class Blockchain:
     # group of block
@@ -9,7 +10,6 @@ class Blockchain:
         self.chain = []
         # genesis block
         self.create_block(nonce=1, previous_hash="0")
-        self.create_block(nonce=11, previous_hash="00")
 
     # build block for keep in blockchain
     def create_block(self, nonce, previous_hash):
@@ -18,7 +18,7 @@ class Blockchain:
             "index":len(self.chain)+1, 
             "timestamp":str(datetime.datetime.now()), 
             "nonce":nonce, 
-            "previouse_hash":previous_hash,  
+            "previous_hash":previous_hash,  
         }
         self.chain.append(block)
         return block
@@ -34,13 +34,82 @@ class Blockchain:
         # sha-256
         return hashlib.sha256(encode_block).hexdigest() # hex = 16 number base
     
+    def proof_of_work(self, previous_nonce):
+        # get nonce that result target hash => 0000xxxxxxxxxxxxx
+        new_nonce = 1
+        check_proof = False
+
+        # mathematical problems
+        while check_proof is False:
+            # get 16 number base
+            hashOperation = hashlib.sha256(str(new_nonce**2 - previous_nonce**2).encode()).hexdigest()
+            if hashOperation[:4] == "0000":
+                check_proof=True
+            else:
+                new_nonce+=1
+        return new_nonce
+
+    def get_previous_block(self):
+        return self.chain[-1]
+    
+    def hash(self, block):
+        encode_block = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(encode_block).hexdigest()
+    
+    def proof_of_work(self, previous_nonce):
+        new_nonce = 1
+        check_proof = False
+
+        while check_proof is False:
+            hashOperation = hashlib.sha256(str(new_nonce**2 - previous_nonce**2).encode()).hexdigest()
+            if hashOperation[:4] == "0000":
+                check_proof = True
+            else:
+                new_nonce+=1
+        return new_nonce
+    
+# web server
+app = Flask(__name__)
+
 # call to use Blockchain 
 blockchain = Blockchain()
-print(blockchain.chain)
 
-# print(blockchain.get_previous_block())
+# routing
+@app.route('/')
+def hello():
+    return "<p>Hello Blockchain</p>"
 
-# first block encryption
-print(blockchain.hash(blockchain.chain[0]))
-# second block encryption
-print(blockchain.hash(blockchain.chain[1]))
+@app.route('/get_chain', methods = ["GET"])
+def get_chain():
+    response = {
+        "chain": blockchain.chain,  
+        "length": len(blockchain.chain)
+    }
+    return jsonify(response), 200
+
+@app.route('/mining', methods = ["GET"])
+def mining_block():
+    # pow
+    previous_block = blockchain.get_previous_block()
+    previous_nonce = previous_block["nonce"]
+
+    # nonce
+    nonce = blockchain.proof_of_work(previous_nonce)
+    
+    # previous hash block
+    previous_hash = blockchain.hash(previous_block)
+
+    # update new block
+    block = blockchain.create_block(nonce, previous_hash)
+    response = {
+        "message": "Mining Block Compeleted", 
+        "index": block["index"], 
+        "timestamp": block["timestamp"], 
+        "nonce": block["nonce"], 
+        "previous_hash": block["previous_hash"]
+    }
+    return jsonify(response), 200
+
+# run server
+if __name__ == "__main__":
+    app.run()
